64138dc6ead857aa4e4feb2588bbfc8a21c87e08
Donghwi_Park <gptnc5100@gmail.com>
2024-10-16 19:54:02 +0200

    Creating the project and creating the binding to the ViewModel

--- CollectionViewDemo/MVVM/Models/Product.cs
+++ CollectionViewDemo/MVVM/Models/Product.cs
@@ -6,5 +6,14 @@ using System.Threading.Tasks;
 
 namespace CollectionViewDemo.MVVM.Models
 {
-   
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image {  get; set; }
+        public int Stock {  get; set; }
+        public bool HasOffer { get; set; }
+        public decimal OfferPrice {  get; set; }
+
+    }
 }
