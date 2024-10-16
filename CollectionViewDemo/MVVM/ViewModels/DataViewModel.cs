ebe5d59a401db915cc59d4b1e66281abd1a5cc0f
Donghwi_Park <gptnc5100@gmail.com>
2024-10-16 19:52:51 +0200

    Add project files.

---
+++ CollectionViewDemo/MVVM/ViewModels/DataViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CollectionViewDemo.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionViewDemo.MVVM.ViewModels
+{
+    public class DataViewModel
+    {
+        public ObservableCollection<Product> Products { get; set; }
+        public DataViewModel()
+        {
+
+        }
+    }
+}
