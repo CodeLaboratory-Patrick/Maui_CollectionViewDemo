595813c76e99a6aa57831ccaf30885611d660c03
Donghwi_Park <gptnc5100@gmail.com>
2024-10-18 11:39:13 +0200

    Selecting a custom view at runtime for empty

--- CollectionViewDemo/MVVM/Views/EmptyView.xaml.cs
+++ CollectionViewDemo/MVVM/Views/EmptyView.xaml.cs
@@ -8,4 +8,11 @@ public partial class EmptyView : ContentPage
     {
         InitializeComponent();
     }
+
+    private void Switch_Toggled(object sender, ToggledEventArgs e)
+    {
+        var isToggled = e.Value;
+        collectionView.EmptyView = isToggled ? Resources["NoResultsView"] :
+             Resources["ConnectivityIssue"];
+    }
 }
